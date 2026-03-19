423d2fb3a5054af323a8074a9bf1a78816421c3b
wojteq18 <279730@student.pwr.edu.pl>
2026-03-19 11:25:29 +0100

    SW lab v2

---
+++ Systemy_Wbudowane/Lab/Lista_2/lab2_wheelsBasic.ino
@@ -0,0 +1,57 @@
+#include "Wheels.h"
+
+
+
+Wheels w;
+bool isInMove = false;
+volatile char cmd;
+
+void setup() {
+  // put your setup code here, to run once:
+  w.attach(4,5,6,7,10,11);
+  
+  Serial.begin(9600);
+  Serial.println("Forward: WAD");
+  Serial.println("Back: ZXC");
+  Serial.println("Stop: S");
+
+}
+
+void loop() {
+  while(Serial.available())
+  {
+    cmd = Serial.read();
+    switch(cmd)
+    {
+      case 'w': w.forward(); break;
+      case 'x': w.back(); break;
+      case 'a': w.forwardLeft(); break;
+      case 'd': w.forwardRight(); break;
+      case 'z': w.backLeft(); break;
+      case 'c': w.backRight(); break;
+      case 's': w.stop(); break;
+      case '1': w.setSpeedLeft(75); break;
+      case '2': w.setSpeedLeft(200); break;
+      case '9': w.setSpeedRight(75); break;
+      case '0': w.setSpeedRight(200); break;
+      case '5': w.setSpeed(100); break;
+      case 'q': forwardBack(); break;
+      case 'p': isInMove = !isInMove;
+    }
+  }
+}
+
+void forwardBack() {
+  w.setSpeedRight(200);
+  w.setSpeedLeft(200);
+  while (!isInMove) {
+    w.forward();
+    delay(1000);
+    w.back();
+    delay(50);
+    w.back();
+    delay(1000);
+    w.forward();
+    delay(50);
+  }
+}
